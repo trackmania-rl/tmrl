9afa15a0bb79186d90222be32844a8ceb0217789
LAPTOP-ABVN6NQF\edoua <edouard.geze@hotmail.fr>
2020-10-15 23:55:25 -0400

    delete useless code

--- gym-tmrl/gym_tmrl/envs/main.py
+++
@@ -1,9 +0,0 @@
-# import library
-
-from gym_tmrl.envs.read_game import screen_record
-
-if __name__ == '__main__':
-    print("Wait 2 sec")
-    #time.sleep(2)
-    print("Go")
-    screen_record(tool=["fps","vis","get_speed", "road", "radar"])
