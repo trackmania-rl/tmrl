ef06c6d217f13f3d0b3310588e1940f966142803
LAPTOP-ABVN6NQF\edoua <edouard.geze@hotmail.fr>
2020-11-04 23:29:28 -0500

    update memorie

--- agents-rt/agents/custom/custom_memories.py
+++ agents-rt/agents/custom/custom_memories.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from agents.memory_dataloading import MemoryDataloading
+import os
 
 
 # LOCAL BUFFER COMPRESSION ==============================
@@ -93,23 +94,14 @@ class MemoryTMNFLidar(MemoryTMNF):
         So we load 5 images from here...
         Don't forget the info dict for CRC debugging
         """
-        # print(f"DEBUG: getitem ---")
         idx_last = item + self.imgs_obs-1
         idx_now = item + self.imgs_obs
-        # print(f"DEBUG: item:{item}, idx_last:{idx_last}, idx_now:{idx_now}")
         imgs = self.load_imgs(item)
-        # print(f"DEBUG: imgs:{imgs}")
         last_act = self.data[1][idx_last]
-        # print(f"DEBUG WARNING!!!: last_act:{last_act}")
         last_obs = (self.data[2][idx_last], imgs[:-1], last_act) if self.act_in_obs else (self.data[2][idx_last], imgs[:-1])
-        # print(f"DEBUG: last_obs:{last_obs}")
         rew = np.float32(self.data[5][idx_now])
-        # print(f"DEBUG: rew:{rew}")
         new_act = self.data[1][idx_now]
-        # print(f"DEBUG WARNING!!!: new_act:{new_act}")
         new_obs = (self.data[2][idx_now], imgs[1:], new_act) if self.act_in_obs else (self.data[2][idx_now], imgs[1:])
-        # print(f"DEBUG: new_obs:{new_obs}")
-        # done = np.float32(self.data[4][idx_now])
         done = self.data[4][idx_now]
         info = self.data[6][idx_now]
         return last_obs, new_act, rew, new_obs, done, info
@@ -118,33 +110,21 @@ class MemoryTMNFLidar(MemoryTMNF):
         res = []
         for i in range(item, item+self.imgs_obs+1):
             img = self.data[3][i]
-            # print(f"DEBUG:i:{i}/{self.__len__()}, img:{img}")
             res.append(img)
         return np.stack(res)
 
     def append_buffer(self, buffer):
         """
-        buffer is a list of samples (obs, act, rew, done, info)
+        buffer is a list of samples ( act, obs, rew, done, info)
         don't forget to keep the info dictionary in the sample for CRC debugging
         """
-        # print(f"DEBUG: appending buffer to replay memory")
-        # print(f"DEBUG: len(self.data):{len(self.data)}, len(buffer):{len(buffer)}")
-
-        # print(f"DEBUG: self.data[0][-1]:{self.data[0][-1]}")
-        # print(f"DEBUG: len(self.data[0]):{len(self.data[0])}")
-        # print(f"DEBUG: type(self.data[0]):{type(self.data[0])}, type(self.data[0][0]):{type(self.data[0][0])}")
-        # print(f"DEBUG: type(self.data[1]):{type(self.data[1])}, type(self.data[1][0]):{type(self.data[1][0])}, type(buffer.memory[0][1]):{type(buffer.memory[0][1])}")
-        # print(f"DEBUG: type(self.data[2]):{type(self.data[2])}, type(self.data[2][0]):{type(self.data[2][0])}, type(buffer.memory[0][0][0]):{type(buffer.memory[0][0][0])}")
-        # print(f"DEBUG: type(self.data[3]):{type(self.data[3])}, type(self.data[3][0]):{type(self.data[3][0])}, type(buffer.memory[0][0][1]):{type(buffer.memory[0][0][1])}")
-        # print(f"DEBUG: type(self.data[4]):{type(self.data[4])}, type(self.data[4][0]):{type(self.data[4][0])}, type(buffer.memory[0][3]):{type(buffer.memory[0][3])}")
-        # print(f"DEBUG: type(self.data[5]):{type(self.data[5])}, type(self.data[5][0]):{type(self.data[5][0])}, type(buffer.memory[0][2]):{type(buffer.memory[0][2])}")
 
         first_data_idx = self.data[0][-1] + 1 if self.__len__() > 0 else 0
 
         d0 = [first_data_idx + i for i, _ in enumerate(buffer.memory)]  # indexes
         d1 = [b[0] for b in buffer.memory]  # actions
         d2 = [b[1][0] for b in buffer.memory]  # speeds
-        d3 = [b[1][1] for b in buffer.memory]  # images
+        d3 = [b[1][1] for b in buffer.memory]  # lidar
         d4 = [b[3] for b in buffer.memory]  # dones
         d5 = [b[2] for b in buffer.memory]  # rewards
         d6 = [b[4] for b in buffer.memory]  # infos
@@ -177,9 +157,6 @@ class MemoryTMNFLidar(MemoryTMNF):
             self.data[5] = self.data[5][to_trim:]
             self.data[6] = self.data[6][to_trim:]
 
-        # print(f"DEBUG: self.stat_train_return:{self.stat_train_return}, self.stat_test_return:{self.stat_test_return}")
-        # else:
-        #     print(f"DEBUG: empty buffer")
         return self
 
 
@@ -199,6 +176,54 @@ class MemoryTM2020(MemoryDataloading):  # TODO
         super().__init__(memory_size, batchsize, device, path_loc, remove_size, obs_preprocessor, sample_preprocessor)
 
     def append_buffer(self, buffer):
+        """
+        buffer is a list of samples (act, obs, rew, done, info)
+        don't forget to keep the info dictionary in the sample for CRC debugging
+        """
+
+        first_data_idx = self.data[0][-1] + 1 if self.__len__() > 0 else 0
+        d0 = [first_data_idx + i for i, _ in enumerate(buffer.memory)]  # indexes
+        d1 = [b[0] for b in buffer.memory]  # actions
+        d2 = [b[1][0] for b in buffer.memory]  # speeds
+        d3 = [b[1][1] for b in buffer.memory]  # gear
+        d4 = [b[1][2] for b in buffer.memory]  # rpm
+        for bi, di in zip(buffer.memory, d0):
+            print("shape img before save : ", bi[1][3].shape)
+            cv2.imwrite(str(self.path / str(di) + '.png'), np.moveaxis(bi[1][3], 0, -1))
+        d5 = [b[3] for b in buffer.memory]  # dones
+        d6 = [b[2] for b in buffer.memory]  # rewards
+        d7 = [b[4] for b in buffer.memory]  # infos
+
+        if self.__len__() > 0:
+            self.data[0] += d0
+            self.data[1] += d1
+            self.data[2] += d2
+            self.data[3] += d3
+            self.data[4] += d4
+            self.data[5] += d5
+            self.data[6] += d6
+            self.data[7] += d7
+        else:
+            self.data.append(d0)
+            self.data.append(d1)
+            self.data.append(d2)
+            self.data.append(d3)
+            self.data.append(d4)
+            self.data.append(d5)
+            self.data.append(d6)
+            self.data.append(d7)
+
+        to_trim = self.__len__() - self.memory_size
+        if to_trim > 0:
+            print(f"DEBUG: trimming {to_trim} elements")
+            self.data[0] = self.data[0][to_trim:]
+            self.data[1] = self.data[1][to_trim:]
+            self.data[2] = self.data[2][to_trim:]
+            self.data[3] = self.data[3][to_trim:]
+            self.data[4] = self.data[4][to_trim:]
+            self.data[5] = self.data[5][to_trim:]
+            self.data[6] = self.data[6][to_trim:]
+            self.data[7] = self.data[7][to_trim:]
         return self
 
     def __len__(self):
@@ -208,23 +233,36 @@ class MemoryTM2020(MemoryDataloading):  # TODO
         idx_last = item + self.imgs_obs - 1
         idx_now = item + self.imgs_obs
         imgs = self.load_imgs(item)
-
-        last_act = np.array(self.data[4][idx_last], dtype=np.float32)
-        last_obs = (
-        np.array([self.data[1][idx_last], ], dtype=np.float32), imgs[:-1], last_act) if self.act_in_obs else (
-        np.array([self.data[1][idx_last], ], dtype=np.float32), imgs[:-1])
+        last_act = np.array(self.data[1][idx_last], dtype=np.float32)
+        if self.act_in_obs:
+            last_obs = (np.array([self.data[2][idx_last], ], dtype=np.float32),
+                        np.array([self.data[3][idx_last], ], dtype=np.float32),
+                        np.array([self.data[4][idx_last], ], dtype=np.float32),
+                        imgs[:-1], last_act)
+        else:
+            last_obs = (np.array([self.data[2][idx_last], ], dtype=np.float32),
+                        np.array([self.data[3][idx_last], ], dtype=np.float32),
+                        np.array([self.data[4][idx_last], ], dtype=np.float32),
+                        imgs[:-1])
         rew = np.float32(self.data[6][idx_now])
-        new_act = np.array(self.data[4][idx_now], dtype=np.float32)
-        new_obs = (np.array([self.data[1][idx_now], ], dtype=np.float32), imgs[1:], new_act) if self.act_in_obs else (
-        np.array([self.data[1][idx_now], ], dtype=np.float32), imgs[1:])
+        new_act = np.array(self.data[1][idx_now], dtype=np.float32)
+        if self.act_in_obs:
+            new_obs = (np.array([self.data[2][idx_now], ], dtype=np.float32),
+                       np.array([self.data[3][idx_now], ], dtype=np.float32),
+                       np.array([self.data[4][idx_now], ], dtype=np.float32),
+                       imgs[:-1], last_act)
+        else:
+            new_obs = (np.array([self.data[2][idx_now], ], dtype=np.float32),
+                       np.array([self.data[3][idx_now], ], dtype=np.float32),
+                       np.array([self.data[4][idx_now], ], dtype=np.float32),
+                       imgs[:-1])
         done = np.float32(self.data[5][idx_now])
-        return last_obs, new_act, rew, new_obs, done
+        info = self.data[idx_now]
+        return last_obs, new_act, rew, new_obs, done, info
 
     def load_imgs(self, item):
         res = []
         for i in range(item, item+self.imgs_obs+1):
             img = cv2.imread(str(self.path / (str(self.data[0][i]) + ".png")))
-            img = img[20:-30, :]
-            img = img.astype('float32') / 255.0
             res.append(np.moveaxis(img, -1, 0))
         return np.array(res)
