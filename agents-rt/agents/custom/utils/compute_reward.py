abc7c2702d3c3d2f3bd61913a37fd1b6869711d8
yannbouteiller <yann.bouteiller@hotmail.fr>
2021-01-21 21:46:30 -0500

    More Markovian reward

--- agents-rt/agents/custom/utils/compute_reward.py
+++ agents-rt/agents/custom/utils/compute_reward.py
@@ -3,11 +3,12 @@ import pickle
 
 
 class RewardFunction:
-    def __init__(self, reward_data_path, nb_obs_forward=10):
+    def __init__(self, reward_data_path, nb_obs_forward=10, nb_obs_backward=10):
         with open(reward_data_path, 'rb') as f:
             self.data = pickle.load(f)
         self.cur_idx = 0
         self.nb_obs_forward = nb_obs_forward
+        self.nb_obs_backward = nb_obs_backward
 
     def compute_reward(self, pos):
         min_dist = np.inf
@@ -24,6 +25,20 @@ class RewardFunction:
             if index == len(self.data) or temp == 0:
                 break
         reward = best_index - self.cur_idx
+        if best_index == self.cur_idx:  # if the best index didn't change, we rewind (more Markovian reward)
+            min_dist = np.inf
+            index = self.cur_idx
+            while True:
+                dist = np.linalg.norm(pos - self.data[index])
+                if dist <= min_dist:
+                    min_dist = dist
+                    best_index = index
+                    temp = self.nb_obs_backward
+                index -= 1
+                temp -= 1
+                # stop condition
+                if index == 0 or temp == 0:
+                    break
         self.cur_idx = best_index
         return reward
 
--- benchmark_environment.py
+++ benchmark_environment.py
@@ -14,6 +14,7 @@ action_space = spaces.Box(low=-1.0, high=1.0, shape=(3,))
 env_config = DEFAULT_CONFIG_DICT
 env_config["interface"] = TM2020InterfaceLidar
 env_config["benchmark"] = True
+env_config["running_average_factor"] = 0.05
 env = gym.make("rtgym:real-time-gym-v0", config=env_config)
 
 t_d = time.time()
@@ -21,11 +22,12 @@ obs = env.reset()
 for idx in range(NB_STEPS-1):
     act = action_space.sample()
     time.sleep(random.uniform(ACT_COMPUTE_MIN, ACT_COMPUTE_MAX))
-    o, r, d, i = env.step(act)
+    # o, r, d, i = env.step(act)
+    o, r, d, i = env.step(None)
+    print(f"rew:{r}")
 t_f = time.time()
 
 elapsed_time = t_f - t_d
 print(f"benchmark results: {env.benchmarks()}")
 print(f"elapsed time: {elapsed_time}")
 print(f"time-step duration: {elapsed_time / NB_STEPS}")
-
