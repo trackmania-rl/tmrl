b2318ed8d848c1c8e93c08acc2f1a7ceebb80d96
yannbouteiller <yann.bouteiller@hotmail.fr>
2021-02-17 16:32:17 -0500

    edouard CC

--- tmrl/custom/config_constants.py
+++ tmrl/custom/config_constants.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # HIGH-LEVEL PRAGMAS: ==========================================
 
-PRAGMA_EDOUARD_YANN_CC = 1  # 2 if ComputeCanada, 1 if Edouard, 0 if Yann  # TODO: remove for release
+PRAGMA_EDOUARD_YANN_CC = 2  # 2 if ComputeCanada, 1 if Edouard, 0 if Yann  # TODO: remove for release
 PRAGMA_SERVER_ON_EDOUARD_YANN = 1  # 1 is server on Edouard's PC, 0 if server on Yann's PC
 PRAGMA_TM2020_TMNF = True  # True if TM2020, False if TMNF
 PRAGMA_LIDAR = True  # True if Lidar, False if images
