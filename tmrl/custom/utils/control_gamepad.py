899cd37d649d96dca8945431778eba63168bab9f
LAPTOP-ABVN6NQF\edoua <edouard.geze@hotmail.fr>
2021-02-19 11:28:34 -0500

    Analog gas and break

--- tmrl/custom/utils/control_gamepad.py
+++ tmrl/custom/utils/control_gamepad.py
@@ -7,13 +7,13 @@ if platform.system() == "Windows":
     def control_gamepad(gamepad, control):
         assert all(-1.0 <= c <= 1.0 for c in control), "This function accepts only controls between -1.0 and 1.0"
         if control[0] > 0:  # gas
-            gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+            gamepad.right_trigger_float(value_float=control[0])
         else:
-            gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+            gamepad.right_trigger_float(value_float=0.0)
         if control[1] > 0:  # break
-            gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
+            gamepad.left_trigger_float(value_float=control[1])
         else:
-            gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
+            gamepad.left_trigger_float(value_float=0.0)
         gamepad.left_joystick_float(control[2], 0.0)  # turn
         gamepad.update()
 
