7668da8336eb661c1c7d1b57cc48c39663ed455a
yannbouteiller <yann.bouteiller@hotmail.fr>
2021-03-12 23:46:43 -0500

    3080

--- tmrl/custom/config_constants.py
+++ tmrl/custom/config_constants.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # HIGH-LEVEL PRAGMAS: ==========================================
 
-PRAGMA_EDOUARD_YANN_CC = 0  # 3 if MISTlab RTX3080, 2 if ComputeCanada, 1 if Edouard, 0 if Yann  # TODO: remove for release
+PRAGMA_EDOUARD_YANN_CC = 3  # 3 if MISTlab RTX3080, 2 if ComputeCanada, 1 if Edouard, 0 if Yann  # TODO: remove for release
 PRAGMA_SERVER_ON_EDOUARD_YANN = 0  # 1 is server on Edouard's PC, 0 if server on Yann's PC
 RUN_NAME = "SAC_4_LIDAR_Yann_new_map_new_code_05_test"  # Yann en cours: "SAC_4_LIDAR_Yann_old_map_new_code_04"
 
