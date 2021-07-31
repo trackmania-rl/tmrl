185e740ec9e076363e5338c99707089483bd712f
yannbouteiller <yann.bouteiller@hotmail.fr>
2021-07-31 18:37:10 -0400

    Added script to explore commits

---
+++ tools/display_commits_size.sh
@@ -0,0 +1,8 @@
+# adapted from https://stackoverflow.com/questions/10622179/how-to-find-identify-large-commits-in-git-history
+# this bash script displays sorted commit sizes in a human-readable fashion
+git rev-list --objects --all |
+  git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' |
+  sed -n 's/^blob //p' |
+  sort --numeric-sort --key=2 |
+  # cut -c 1-12,41- |
+  $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
