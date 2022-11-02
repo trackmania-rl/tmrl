e96551249bca98dfc652ea856617a1968091992e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 03:08:51 +0000

    Adding tarfile member sanitization to extractall()

--- tmrl/custom/custom_checkpoints.py
+++ tmrl/custom/custom_checkpoints.py
@@ -27,7 +27,26 @@ def load_run_instance_images_dataset(checkpoint_path):
     logging.debug(f" load: tar_path :{tar_path}")
     logging.debug(f" load: dataset_path :{dataset_path}")
     with tarfile.open(tar_path, 'r') as t:
-        t.extractall(dataset_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, dataset_path)
     return load(checkpoint_path)
 
 
