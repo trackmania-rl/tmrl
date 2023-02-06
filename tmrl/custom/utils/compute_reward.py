a0df34ea74d442929b58fedb1472842beb96da57
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2023-02-05 22:08:50 -0500

    Reward shaping

--- tmrl/custom/utils/compute_reward.py
+++ tmrl/custom/utils/compute_reward.py
@@ -9,46 +9,88 @@ import logging
 
 class RewardFunction:
     """
-    Computes a reward from the Openplanet API for Trackmania 2020
+    Computes a reward from the Openplanet API for Trackmania 2020.
     """
-    def __init__(self, reward_data_path, nb_obs_forward=10, nb_obs_backward=10, nb_zero_rew_before_failure=10, min_nb_steps_before_failure=int(3.5 * 20)):
+    def __init__(self,
+                 reward_data_path,
+                 nb_obs_forward=10,
+                 nb_obs_backward=10,
+                 nb_zero_rew_before_failure=10,
+                 min_nb_steps_before_failure=int(3.5 * 20),
+                 max_dist_from_traj=60.0):
+        """
+        Instantiates a reward function for TM2020.
+
+        Args:
+            reward_data_path: path where the trajectory file is stored
+            nb_obs_forward: max distance of allowed cuts (as a number of positions in the trajectory)
+            nb_obs_backward: same thing but for when rewinding the reward to a previously visited position
+            nb_zero_rew_before_failure: after this number of steps with no reward, episode is terminated
+            min_nb_steps_before_failure: the episode must have at least this number of steps before failure
+            max_dist_from_traj: the reward is 0 if the car is further than this distance from the demo trajectory
+        """
         if not os.path.exists(reward_data_path):
             logging.debug(f" reward not found at path:{reward_data_path}")
             self.data = np.array([[0.0, 0.0, 0.0], [1.0, 1.0, 1.0]])  # dummy reward
         else:
             with open(reward_data_path, 'rb') as f:
                 self.data = pickle.load(f)
+
         self.cur_idx = 0
         self.nb_obs_forward = nb_obs_forward
         self.nb_obs_backward = nb_obs_backward
         self.nb_zero_rew_before_failure = nb_zero_rew_before_failure
         self.min_nb_steps_before_failure = min_nb_steps_before_failure
+        self.max_dist_from_traj = max_dist_from_traj
         self.step_counter = 0
         self.failure_counter = 0
         self.datalen = len(self.data)
 
+        # self.traj = []
+
     def compute_reward(self, pos):
+        """
+        Computes the current reward given the position pos
+        Args:
+            pos: the current position
+        Returns:
+            float, bool: the reward and the terminated signal
+        """
+        # self.traj.append(pos)
+
         terminated = False
-        self.step_counter += 1
-        min_dist = np.inf
-        index = self.cur_idx
-        temp = self.nb_obs_forward
-        best_index = 0
+        self.step_counter += 1  # step counter to enable failure counter
+        min_dist = np.inf  # smallest distance found so far in the trajectory to the target pos
+        index = self.cur_idx  # cur_idx is where we were last step in the trajectory
+        temp = self.nb_obs_forward  # counter used to find cuts
+        best_index = 0  # index best matching the target pos
+
         while True:
-            dist = np.linalg.norm(pos - self.data[index])
-            if dist <= min_dist:
-                min_dist = dist
-                best_index = index
-                temp = self.nb_obs_forward
-            index += 1
-            temp -= 1
+            dist = np.linalg.norm(pos - self.data[index])  # distance of the current index to target pos
+            if dist <= min_dist:  # if dist is smaller than our minimum found distance so far,
+                min_dist = dist  # then we found a new best distance,
+                best_index = index  # and a new best index
+                temp = self.nb_obs_forward  # we will have to check this number of positions to find a possible cut
+            index += 1  # now we will evaluate the next index in the trajectory
+            temp -= 1  # so we can decrease the counter for cuts
             # stop condition
-            if index >= self.datalen or temp <= 0:
-                break
+            if index >= self.datalen or temp <= 0:  # if trajectory complete or cuts counter depleted
+                # We check that we are not too far from the demo trajectory:
+                if min_dist > self.max_dist_from_traj:
+                    best_index = self.cur_idx  # if so, consider we didn't move
+
+                # print(f"DEBUG: min_dist={min_dist:.3f}, pos:[{pos[0].item():.3f}, {pos[1].item():.3f}, {pos[2].item():.3f}] / [{self.data[best_index][0].item():.3f}, {self.data[best_index][1].item():.3f}, {self.data[best_index][2].item():.3f}], index:{best_index}")
+
+                break  # we found the best index and can break the while loop
+
+        # The reward is then proportional to the number of passed indexes (i.e., track distance):
         reward = (best_index - self.cur_idx) / 100.0
+
         if best_index == self.cur_idx:  # if the best index didn't change, we rewind (more Markovian reward)
             min_dist = np.inf
             index = self.cur_idx
+
+            # Find the best matching index in rewind:
             while True:
                 dist = np.linalg.norm(pos - self.data[index])
                 if dist <= min_dist:
@@ -60,16 +102,32 @@ class RewardFunction:
                 # stop condition
                 if index <= 0 or temp <= 0:
                     break
+
+            # If failure happens for too many steps, the episode terminates
             if self.step_counter > self.min_nb_steps_before_failure:
                 self.failure_counter += 1
                 if self.failure_counter > self.nb_zero_rew_before_failure:
                     terminated = True
-        else:
-            self.failure_counter = 0
-        self.cur_idx = best_index
+
+        else:  # if we did progress on the track
+            self.failure_counter = 0  # we reset the counter triggering episode termination
+
+        self.cur_idx = best_index  # finally, we save our new best matching index
+
         return reward, terminated
 
     def reset(self):
+        """
+        Resets the reward function for a new episode.
+        """
+        # from pathlib import Path
+        # import pickle as pkl
+        # path_traj = Path.home() / 'TmrlData' / 'reward' / 'traj.pkl'
+        # with open(path_traj, 'wb') as file_traj:
+        #     pkl.dump(self.traj, file_traj)
+
         self.cur_idx = 0
         self.step_counter = 0
         self.failure_counter = 0
+
+        # self.traj = []
