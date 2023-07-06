89484203577b27a5d8ead30fb2b8589c0be11a92
pius <tikuser@ee-tik-nb114.ethz.ch>
2023-07-06 17:23:51 +0200

    fullscrean ubisoft connect

--- tmrl/scripts/start_trackmania.py
+++ tmrl/scripts/start_trackmania.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+from fastgrab import screenshot
 
 # Find the window ID for the window with name "Lutris"
 print("find lutris window ...")
@@ -29,21 +30,53 @@ subprocess.run(['xdotool', 'mousemove', '300', '100'])
 subprocess.run(['xdotool', 'click', '1'])
 time.sleep(1)
 
-print("start ubisoft connect ...", end=" ")
+print("start ubisoft connect ... waited for", end=" ")
 subprocess.run(['xdotool', 'mousemove', '250', '600'])
 subprocess.run(['xdotool', 'click', '1'])
 for i in range(90):
     time.sleep(1)
     if i % 10 == 0:
-        print(f"{i}, ")
-print("\tubisoft connect should have opened up by now")
+        print(f"{i}s, ", end="")
+print("ubisoft connect should have opened up by now")
 
 # Find the window ID for the window with name "ubisoft connect"
+print("find ubisoft window ...")
 proc = subprocess.Popen(['xdotool', 'search', '--onlyvisible', '--name', 'ubisoft connect'], stdout=subprocess.PIPE)
 w_ubisoft = proc.stdout.readline().decode().strip()
 print(f"\tubisoft window detected, id={w_ubisoft}")
 
-quit()
+print("fullscreen ubisoft connect ...")
+# Define the range for valid RGB values
+bgr_bounds = [
+    [24, 32],
+    [20, 27],
+    [15, 23]]
+lower_range = np.array([24, 20, 17], dtype=np.uint8)
+upper_range = np.array([26, 22, 19], dtype=np.uint8)
+
+# take screenshot
+grab = screenshot.Screenshot()
+img = grab.capture()[:, :, :-1][:, :, :3]
+
+# Create a mask for pixels outside the range
+mask = cv2.inRange(img, lower_range, upper_range)
+# cv2.imwrite("masked.png", mask)
+edges = cv2.Canny(mask, 100, 256)
+
+# Find contours of the edges
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+if len(contours) != 1:
+    print("WARNING: found more than one top-bar")
+    print("WARNING: might need manual user adjustment of ubisoft connect screen")
+
+for contour in contours:
+    x, y, w, h = cv2.boundingRect(contour)
+    print(f"\tfound bar at ({x}, {y}) with width {w} and height {h}")
+    print("\tdouble tap bar")
+    subprocess.run(['xdotool', 'mousemove', str(x + 5), str(y + 5)])
+    subprocess.run(['xdotool', 'click', '1'])
+    subprocess.run(['xdotool', 'click', '1'])
+    time.sleep(1)
 
 print("enter games ...")
 subprocess.run(['xdotool', 'mousemove', '200', '50'])
