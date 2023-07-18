1fbc4b2eac87336e04a211955583e0c3bcb6354b
Pius <piuskri@gmail.com>
2023-07-18 10:38:45 +0200

    delete scripts

--- scripts/reset_environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "kill old processes ..."
-sudo pkill Xvfb
-sudo fuser -k $PORT/tcp
-tmux kill-session -t lutris
-tmux kill-session -t xvfb-server
--- scripts/start_vnc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "start tmux-session vnc-server for display $DISPLAY on port $PORT"
-x11vnc -display $DISPLAY -rfbport $PORT -forever -shared &
-
