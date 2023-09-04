1c95d5c7110d9f7f03c5a94dde2e48161626234f
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2023-09-04 15:03:07 -0400

    reindent

--- tmrl/logger.py
+++ tmrl/logger.py
@@ -6,12 +6,13 @@ import logging
 LOG_FORMAT = "[%(levelname)-5s] - %(name)-15s - %(funcName)15s() - %(message)s"
 LOG_LEVEL = logging.INFO
 
+
 def setup_logger(logger):
-        for handler in logger.handlers:
-            logger.removeHandler(handler)
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(LOG_LEVEL)
-        formatter = logging.Formatter(LOG_FORMAT)
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
-        logger.setLevel(LOG_LEVEL)
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(LOG_LEVEL)
+    formatter = logging.Formatter(LOG_FORMAT)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.setLevel(LOG_LEVEL)
