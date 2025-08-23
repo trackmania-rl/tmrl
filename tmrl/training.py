3651ef947bd5605c7edfd2a66f7ce4a9a7709c05
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2025-08-23 12:08:12 -0400

    typo

--- tmrl/training.py
+++ tmrl/training.py
@@ -27,7 +27,7 @@ class TrainingAgent(ABC):
         Executes a training step.
 
         Args:
-            batch: tuple or batched tensors (previous observation, action, reward, new observation, terminated, truncated)
+            batch: tuple of batched tensors (previous observation, action, reward, new observation, terminated, truncated)
 
         Returns:
             dict: a dictionary containing one entry per metric you wish to log (e.g. for wandb)
